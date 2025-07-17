0746d31a6c61f495d8e5d6786eafcd55980d865b
codewithprags <pragati.dwivedi@outlook.com>
2025-07-17 19:02:03 -0400

    adding note book and loading the data set

---
+++ dataset.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from ucimlrepo import fetch_ucirepo
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+"""This script fetches the diabetes dataset from the UCI Machine Learning Repository"""
+#diabetes_130_us_hospitals_for_years_1999_2008 = fetch_ucirepo(id=296) 
+  
+# data (as pandas dataframes) 
+#X = diabetes_130_us_hospitals_for_years_1999_2008.data.features 
+#y = diabetes_130_us_hospitals_for_years_1999_2008.data.targets 
+  
+# metadata 
+#print(diabetes_130_us_hospitals_for_years_1999_2008.metadata) 
+
+#save as one combined csv
+#combined_df = pd.concat([X, y], axis=1)
+#combined_df.to_csv('diabetes_130_us_hospitals_combined.csv', index=False)
+
+
+
+
+
+
